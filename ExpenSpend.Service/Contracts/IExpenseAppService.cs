c699659d51c5ffda461c071467154c7d8e9fe728
Rahul Kumar <r1303yadav@gmail.com>
2024-05-14 11:08:57 +0530

    Add expense functionality to app

---
+++ ExpenSpend.Service/Contracts/IExpenseAppService.cs
@@ -0,0 +1,76 @@
+﻿using ExpenSpend.Domain.DTOs.Expenses;
+using ExpenSpend.Service.Models;
+
+public interface IExpenseAppService
+{
+    /// <summary>
+    /// Retrieves all expenses.
+    /// </summary>
+    /// <returns>A response containing a list of expenses.</returns>
+    Task<Response> GetAllExpensesAsync();
+
+    /// <summary>
+    /// Retrieves an expense by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the expense to retrieve.</param>
+    /// <returns>A response containing the expense details.</returns>
+    Task<Response> GetExpenseByIdAsync(Guid id);
+
+    /// <summary>
+    /// Retrieves the payments associated with a specific expense.
+    /// </summary>
+    /// <param name="id">The ID of the expense.</param>
+    /// <returns>A response containing a list of payments for the expense.</returns>
+    Task<Response> GetExpensePaymentsAsync(Guid id);
+
+    /// <summary>
+    /// Retrieves all expenses for a specific group.
+    /// </summary>
+    /// <param name="groupId">The ID of the group.</param>
+    /// <returns>A response containing a list of expenses for the group.</returns>
+    Task<Response> GetGroupExpensesAsync(Guid groupId);
+
+    /// <summary>
+    /// Retrieves all expenses for a specific user.
+    /// </summary>
+    /// <param name="userId">The ID of the user.</param>
+    /// <returns>A response containing a list of expenses for the user.</returns>
+    Task<Response> GetUserExpensesAsync(Guid userId);
+
+    /// <summary>
+    /// Retrieves all expenses for a specific user within a specific group.
+    /// </summary>
+    /// <param name="userId">The ID of the user.</param>
+    /// <param name="groupId">The ID of the group.</param>
+    /// <returns>A response containing a list of expenses for the user within the group.</returns>
+    Task<Response> GetUserGroupExpensesAsync(Guid userId, Guid groupId);
+
+    /// <summary>
+    /// Creates a new expense.
+    /// </summary>
+    /// <param name="createExpenseDto">The data transfer object containing the details of the expense to create.</param>
+    /// <returns>A response indicating the result of the operation.</returns>
+    Task<Response> CreateExpenseAsync(CreateExpenseDto createExpenseDto);
+
+    /// <summary>
+    /// Updates an existing expense.
+    /// </summary>
+    /// <param name="id">The ID of the expense to update.</param>
+    /// <param name="updateExpenseDto">The data transfer object containing the updated details of the expense.</param>
+    /// <returns>A response indicating the result of the operation.</returns>
+    Task<Response> UpdateExpenseAsync(Guid id, UpdateExpenseDto updateExpenseDto);
+
+    /// <summary>
+    /// Deletes an expense.
+    /// </summary>
+    /// <param name="id">The ID of the expense to delete.</param>
+    /// <returns>A response indicating the result of the operation.</returns>
+    Task<Response> DeleteExpenseAsync(Guid id);
+
+    /// <summary>
+    /// Settles an expense.
+    /// </summary>
+    /// <param name="id">The ID of the expense to settle.</param>
+    /// <returns>A response indicating the result of the operation.</returns>
+    Task<Response> SettleExpenseAsync(Guid id);
+}
---
+++ ExpenSpend.Web/Controllers/ExpenseController.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using ExpenSpend.Domain.DTOs.Expenses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace ExpenSpend.Web.Controllers
+{
+    [Route("api/expenses")]
+    [ApiController]
+    [Authorize]
+    public class ExpenseController : ControllerBase
+    {
+        private readonly IExpenseAppService _expenseService;
+        private readonly IMapper _mapper;
+
+        public ExpenseController(IExpenseAppService expenseService, IMapper mapper)
+        {
+            _expenseService = expenseService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllExpenses()
+        {
+            var expenses = await _expenseService.GetAllExpensesAsync();
+            if (expenses.IsSuccess)
+            {
+                return Ok(expenses.Data);
+            }
+            return NotFound(expenses);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetExpenseById(Guid id)
+        {
+            var expense = await _expenseService.GetExpenseByIdAsync(id);
+            if (expense.IsSuccess)
+            {
+                return Ok(expense.Data);
+            }
+            return NotFound(expense);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateExpense(CreateExpenseDto input)
+        {
+            var result = await _expenseService.CreateExpenseAsync(input);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateExpense(Guid id, UpdateExpenseDto input)
+        {
+            var result = await _expenseService.UpdateExpenseAsync(id, input);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExpense(Guid id)
+        {
+            var result = await _expenseService.DeleteExpenseAsync(id);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result);
+        }
+    }
+}
