0a6379d77c2a115ca35f8ee6c7d56604814aca5b
Rahul Kumar <r1303yadav@gmail.com>
2024-05-14 10:54:18 +0530

    Add expense service

---
+++ ExpenSpend.Domain/DTOs/Expenses/UpdateExpenseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenSpend.Domain.DTOs.Expenses
+{
+    public class UpdateExpenseDto
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public double Amount { get; set; }
+        public bool IsSettled { get; set; }
+    }
+}
